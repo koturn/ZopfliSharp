af3ab46d1496576a53e83197c934b061a5073287
koturn <jeak.koutan.apple@gmail.com>
2022-02-25 20:11:00 +0900

    Move enum definitions to new files

--- ZopfliSharp/Zopfli.cs
+++ ZopfliSharp/Zopfli.cs
@@ -229,24 +229,4 @@ namespace ZopfliSharp
             return compressedDataHandle;
         }
     }
-
-
-    /// <summary>
-    /// Output binary format types.
-    /// </summary>
-    public enum ZopfliFormat
-    {
-        /// <summary>
-        /// Output to gzip format.
-        /// </summary>
-        GZip,
-        /// <summary>
-        /// Output to zlib format.
-        /// </summary>
-        ZLib,
-        /// <summary>
-        /// Output to deflate format
-        /// </summary>
-        Deflate
-    }
 }
---
+++ ZopfliSharp/ZopfliFormat.cs
@@ -0,0 +1,21 @@
+﻿namespace ZopfliSharp
+{
+    /// <summary>
+    /// Output binary format types.
+    /// </summary>
+    public enum ZopfliFormat
+    {
+        /// <summary>
+        /// Output to gzip format.
+        /// </summary>
+        GZip,
+        /// <summary>
+        /// Output to zlib format.
+        /// </summary>
+        ZLib,
+        /// <summary>
+        /// Output to deflate format
+        /// </summary>
+        Deflate
+    }
+}
